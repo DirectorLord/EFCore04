4a73ff014436e94e8633c0fa84115274c1b8bb36
DirectorLord <thelord161203@gmail.com>
2025-08-31 23:19:18 +0300

    finished the demo

--- EFCore04-2/Program.cs
+++ EFCore04-2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace EFCore04_2;
+
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using EFCore04_2.Context;
+
+internal class Program
+{
+    public static async Task Main()
+    {
+        var context = new NorthwindContext(); //idk whyy is there an error here
+        #region DB first
+        //foreach (var item in context.Employees)
+        //{
+        //    Console.WriteLine(item.FirstName);
+        //}
+        #endregion
+
+        #region Stored Procedure
+        context.Procedures.TenMostExpensiveProductsAsync();
+
+        var result = context.Procedures.SelectAllProductsAsync();
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item.ProductName);
+        }
+        #endregion
+
+    }
+}
