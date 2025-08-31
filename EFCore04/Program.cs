4647993e7b2a1bcbb7c3c0f7ea8da6beb5dc54a4
DirectorLord <thelord161203@gmail.com>
2025-08-31 19:06:13 +0300

    test

--- EFCore04/Program.cs
+++ EFCore04/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace EFCore04;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.ReadLine();
+        
+    }
+}
